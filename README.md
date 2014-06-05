392a22292d606f45cbbb56c5325cccadd39a28d1
Bronson Quick <bronson@sennza.com.au>
2014-06-05 12:10:12 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Sennza Maintenance Helper For Mailchimp
+
+Once a month at Sennza we send out maintenance emails to clients in Mailchimp that inform our clients of the core and plugin updates we have completed on their websites as part of their maintenance plan.
+
+Altering the markup that is generated on the WordPress Updates screen was time consuming so we've created this plugin to generate the markup for us so we can paste it straight into MailChimp.
+
+##Installing
+
+1. Down the zip from Github.
+2. Upload the zip.
+3. Activate the plugin and browse to Settings->Maintenance Helper.
+
+##Usage for Maintenance
+
+1. Create a staging site
+2. Upgrade core, plugins and themes.
+3. Test the major features of the site to ensure that the plugins were update successfully and nothing was broken.
+4. If the updates were successful then install and activate this plugin on the live site.
+5. Browse to Settings->Maintenance Helper.
+6. Copy and paste the HTML into MailChimp.
+7. Backup the main site.
+8. Upgrade the site.
+9. Send the Mailchimp Maintenance to the clients.
+10. Rejoice.
+
+##Screenshot
+
---
+++ sennza-maintenance-helper.php
@@ -0,0 +1,95 @@
+<?php
+/*
+Plugin Name: Sennza Maintenance Helper
+Plugin URI: http://www.sennza.com.au
+Description: This plugin generates the html for WordPress plugins, theme and core updates that we can paste into our Mailchimp maintenance email that we send out to clients.
+Version: 1.0
+Author: Bronson Quick
+Author URI: http://www.sennza.com.au
+License: GPL2
+*/
+
+class Sennza_Maintenance_Helper {
+	private static $instance;
+
+	static function get_instance() {
+		if ( ! self::$instance )
+			self::$instance = new Sennza_Maintenance_Helper;
+
+		return self::$instance;
+	}
+
+	private function __construct() {
+		add_action( 'admin_menu', array( $this, 'add_options_page' ) );
+	}
+
+	public function add_options_page() {
+		add_options_page( 'Maintenance Helper', 'Maintenance Helper', 'manage_options', 'maintenance-helper.php', array( $this, 'generate_mailchimp_markup' ) );
+	}
+
+	public function generate_mailchimp_markup() { ?>
+
+		<div class="wrap">
+			<h1>Maintenance Helper</h1>
+			<?php
+				$content = $this->get_maintenance_markup();
+				$editor_id = 'maintenancehelper';
+				$settings = array(
+					'teeny'         => true,
+					'media_buttons' => false,
+				);
+				wp_editor( $content, $editor_id, $settings );
+			?>
+	    </div>
+	<?php
+	}
+
+	public function get_maintenance_markup() {
+
+		$markup = '<h3>Plugin &amp; WordPress Core Updates</h3>';
+
+		global $wp_version;
+
+		$updates = get_core_updates();
+
+		if ( ! empty ( $updates ) ) {
+			$markup .= 'We have upgraded WordPress Core:';
+			$markup .= "<ul>";
+
+			foreach( (array) $updates as $update ) {
+
+				$markup .= "<li>From WordPress Version: " . esc_attr( $wp_version ) . " to WordPress ". $update->current . " on " . date( 'd F Y' ) . "</li>";
+			}
+			$markup .= "</ul>";
+		}
+
+		$plugins = get_plugin_updates();
+
+		if ( ! empty ( $plugins ) ) {
+			$markup .= "We have upgrade the following plugins on your site:";
+			$markup .= "<ul>";
+			foreach ( (array) $plugins as $plugin_file => $plugin_data ) {
+
+				$details_url = self_admin_url('plugin-install.php?tab=plugin-information&plugin=' . $plugin_data->update->slug . '&section=changelog&TB_iframe=true&width=640&height=662');
+
+				$markup .= "<li>";
+				$markup .= $plugin_data->Name . ': ';
+				$markup .= sprintf( __( 'Version %1$s to <a href="%2$s">%3$s</a>.' ), $plugin_data->Version, esc_url($details_url), $plugin_data->update->new_version );
+				$markup .= "</li>";
+
+			}
+			$markup .= "</ul>";
+		}
+
+		$markup .= "<h3>Support For The Month of *|DATE:F Y|*</h3>";
+		$markup .= "During the month of *|DATE:F Y|* we&#39;ve helped you with the following support requests:";
+		$markup .= "<ul>";
+		$markup .= "<li>Email support</li>";
+		$markup .= "</ul>";
+		$markup .= "<h3 class='null'><span class='mc-toc-title'>Want to learn more?</span></h3>";
+		$markup .= "If you have any questions about your plan or the work our team have performed on your site please feel free to contact us.<br />";
+
+		return $markup;
+	}
+}
+add_action( 'plugins_loaded', array( 'Sennza_Maintenance_Helper' , 'get_instance' ) );
